7078e7b2fcdc9e1ae49ceaa3178d6becae303b89
Julie Brosse <privatejuliedev@gmail.com>
2020-05-04 18:24:46 +0200

    added medium page commands

---
+++ back/index.js
@@ -0,0 +1,46 @@
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      jwt = require('jsonwebtoken'),
+      config = require('./configs/config'),
+      app = express();
+// 1
+app.set('llave', config.llave);
+// 2
+app.use(bodyParser.urlencoded({ extended: true }));
+// 3
+app.use(bodyParser.json());
+// 4
+app.listen(3000,()=>{
+    console.log('Servidor iniciado en el puerto 3000') 
+});
+// 5
+app.get('/', function(req, res) {
+    res.send('Inicio');
+});
+
+app.post('/autenticar', (req, res) => {
+    if(req.body.usuario === "asfo" && req.body.contrasena === "holamundo") {
+  const payload = {
+   check:  true
+  };
+  const token = jwt.sign(payload, app.get('llave'), {
+   expiresIn: 1440
+  });
+  res.json({
+   mensaje: 'Autenticación correcta',
+   token: token
+  });
+    } else {
+        res.json({ mensaje: "Usuario o contraseña incorrectos"})
+    }
+})
+
+app.get('/datos', rutasProtegidas, (req, res) => {
+    const datos = [
+     { id: 1, nombre: "Asfo" },
+     { id: 2, nombre: "Denisse" },
+     { id: 3, nombre: "Carlos" }
+    ];
+    
+    res.json(datos);
+   });
