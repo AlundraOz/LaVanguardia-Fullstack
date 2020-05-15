3e652e7bdcea1ab4658bdf6adaffff5d640cc0bd
Julie Brosse <privatejuliedev@gmail.com>
2020-05-15 11:18:57 +0200

    button login with submenu ok

--- front/src/components/Categories/category.js
+++ front/src/components/Categories/category.js
@@ -4,11 +4,60 @@ import './category.css';
 import logoProfile from './user_info.svg';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
 
 
+
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+  root: {
+    '&:focus': {
+      backgroundColor: theme.palette.primary.main,
+      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+}))(MenuItem);
 
 //Navbar Categories in the game section page
 const Category = (props) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <div>
       <div className="navCategories">
@@ -21,12 +70,46 @@ const Category = (props) => {
           <p>Todos</p>
         </Link>
         <input className="buttonCategories" type="text" placeholder="Search.."/>
-        <Link to='/Access'>
-          <div className="userProfileLogo">
-            <img src={logoProfile} className="imageUserLogin"/>
-            <p>Iniciar Sesión</p>
-          </div>
-        </Link>
+          <div>
+      <Button
+        className="userProfileLogo"
+        aria-controls="customized-menu"
+        aria-haspopup="true"
+        variant="contained"
+        color="primary"
+        onClick={handleClick}
+      >
+        <img src={logoProfile} className="imageUserLogin"/>
+      </Button>
+      <StyledMenu
+        id="customized-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <StyledMenuItem>
+          <Link to='/Access' style={{display: 'flex'}}>
+            <ListItemIcon>
+              <SendIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText primary="Logearme" />
+          </Link>
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemIcon>
+            <DraftsIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Mi perfil" />
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemIcon>
+            <InboxIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Salir de mi perfil" />
+        </StyledMenuItem>
+      </StyledMenu>
+    </div>
       </div>
 
     </div>
