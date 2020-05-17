4d3366fec31163f6d5b9946ab200bf90229a5970
masko-vl <58594257+masko-vl@users.noreply.github.com>
2020-05-17 10:37:33 +0200

    changed back

--- back/configs/config.js
+++ back/configs/config.js
@@ -2,11 +2,7 @@ const  mysql = require('mysql');
 const  connection = mysql.createConnection({
   host :  'localhost', // address of the server
   user :  'root', // username
-<<<<<<< HEAD
-  password :  '327d361f!',
-=======
-  password :  'BRDONJU12',
->>>>>>> 3e652e7bdcea1ab4658bdf6adaffff5d640cc0bd
+  password :  'root',
   database :  'games_database',
 });
 
