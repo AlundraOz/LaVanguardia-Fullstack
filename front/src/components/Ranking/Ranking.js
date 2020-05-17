05793f2e09e8250da5cb319c588624bfa902fed9
AlundraOz <ailuozols@gmail.com>
2020-05-17 09:55:18 +0200

    added modal ranking ,close button working

--- front/src/components/Ranking/Ranking.js
+++ front/src/components/Ranking/Ranking.js
@@ -5,11 +5,64 @@ import {
 } from 'reactstrap';
 import './Ranking.css';
 
+/*export class Ranking extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: true,
+            ranking: []
+         
+        }
+    }
+
+    componentDidMount() {
+        fetch(`http://localhost:5000/ranking/${this.props.game_score}`)
+            .then(res => res.json())
+            .then(data => this.setState({ranking: data})) 
+    }
+
+    toggle = () => this.setState({
+            modal: !this.state.modal
+        })
+    
+   
+  render() {
+
+      return (
+        < div className = "instructionGames" >
+                 <Modal isOpen={this.state.modal} toggle={this.toggle} style={{ marginTop:"10%"}}>
+                 <ModalBody>
+                        {this.state.ranking &&
+                            <table style={{color:'black', zIndex:1000}}>
+                                <thead>
+                                <tr>
+                                    <th>NOMBRE</th>
+                                    <th>PUNTUACIÓN</th>
+                                </tr>
+                                </thead>
+                            <tbody>
+                            {this.state.ranking.map((score, index)=>{
+                                return(
+                                    <tr>
+                                        <td key={shortId.generate()}>{score.name}</td>
+                                        <td key={shortId.generate()}>{Object.values(score)[1]}</td>
+                                    </tr>
+                                )
+                            })}
+                            <button color="primary" onClick={this.toggle}>Close</button>     
+                            </tbody>
+                        </table>}
+                    </ModalBody>
+             </Modal>    
+        </div>
+    )}
+}
+
+export default Ranking;*/
 
 const Ranking=({game_score})=>{
     const [ranking, setRanking]= useState()
     const [modal, setModal] = useState(true);
-    const [closeAll, setCloseAll] = useState(true);
 
     useEffect(()=>{
         fetch(`http://localhost:5000/ranking/${game_score}`)
@@ -19,15 +72,13 @@ const Ranking=({game_score})=>{
            
     },[])
 
-    const toggle = () => setModal(modal);
-    const toggleAll = () => {
-        setCloseAll(true);
-      }
+    const toggle = () => setModal(!modal);
+
     
     console.log(ranking)
     return(
         <Fragment>
-             <Modal isOpen={modal} toggle={toggle} onClosed={closeAll ? toggle : undefined} style={{ marginTop:"10%"}}>
+             <Modal isOpen={modal} toggle={toggle} >
                  <ModalBody>
                         {ranking &&
                             <table style={{color:'black', zIndex:1000}}>
@@ -46,7 +97,7 @@ const Ranking=({game_score})=>{
                                     </tr>
                                 )
                             })}
-                            <button color="primary" onClick={toggleAll}>Close</button>     
+                            <Button color="primary" onClick={toggle}>Close</Button>     
                             </tbody>
                         </table>}
                     </ModalBody>
